c33a4780ff18875da521a024f3f6b5fa3cab7cba
AlexEntrepreneur <hello@alexentrepreneur.com>
2019-06-06 09:32:29 +0100

    binding

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles.css';
 
-const shopping = '';
+const shopping = 'Test';
 
 class App extends React.Component {
   constructor() {
@@ -10,11 +10,19 @@ class App extends React.Component {
       shopping
     };
   }
+  
+  testBinding() {
+    // We have to explicitly provide the value of this or
+    // We can use arrow functions to automatically pass on the value of this
+    console.log("function fired!", this);
+  }
+
   render() {
+    console.log(this.state.shopping)
     return (
       <div className="App">
         <div className="header">
-          <h1>Shopping List</h1>
+          <h1 onClick={this.testBinding.bind({ lemon: true })}>Shopping List</h1>
         </div>
       </div>
     );
