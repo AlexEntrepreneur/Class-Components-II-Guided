cee5f6f4874841a61db646cdd64d65513f5d98f5
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-06-06 01:19:49 +0100

    Add Project Files

---
+++ app/src/App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import './styles.css';
+
+const groceries = [
+  {
+    name: 'Bananas',
+    id: 123,
+    purchased: false
+  },
+  {
+    name: 'Torillas',
+    id: 124,
+    purchased: false
+  },
+  {
+    name: 'Milk',
+    id: 1235,
+    purchased: false
+  },
+  {
+    name: 'Pizza Sauce',
+    id: 1246,
+    purchased: false
+  },
+  {
+    name: 'Raw Honey',
+    id: 1237,
+    purchased: false
+  },
+  {
+    name: 'Granola',
+    id: 1248,
+    purchased: false
+  }
+];
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      groceries
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="header">
+          <h1>Shopping List</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
