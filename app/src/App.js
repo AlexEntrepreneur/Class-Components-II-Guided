db2a3bea293ebb8bea47fc8cc62e038657894d88
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-06-06 01:42:21 +0100

    Remove Dummy Data

--- app/src/App.js
+++ app/src/App.js
@@ -1,44 +1,13 @@
 import React from 'react';
 import './styles.css';
 
-const groceries = [
-  {
-    name: 'Bananas',
-    id: 123,
-    purchased: false
-  },
-  {
-    name: 'Torillas',
-    id: 124,
-    purchased: false
-  },
-  {
-    name: 'Milk',
-    id: 1235,
-    purchased: false
-  },
-  {
-    name: 'Pizza Sauce',
-    id: 1246,
-    purchased: false
-  },
-  {
-    name: 'Raw Honey',
-    id: 1237,
-    purchased: false
-  },
-  {
-    name: 'Granola',
-    id: 1248,
-    purchased: false
-  }
-];
+const shopping = '';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      groceries
+      shopping
     };
   }
   render() {
